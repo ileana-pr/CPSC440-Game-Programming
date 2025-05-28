89f7905f780b40e4b97c91e809a57ca64f31df1d
cheddarqueso787 <cheddarqueso787@gmail.com>
2025-05-27 20:57:52 -0400

    add spaceship code

--- Creating Bitmap Lab/Creating Bitmap Lab/createBitmap.cpp
+++ Creating Bitmap Lab/Creating Bitmap Lab/createBitmap.cpp
@@ -1,19 +1,22 @@
+//Name: Ileana Perez
+//Program: Creating Bitmap Lab
+
 #include <allegro5\allegro.h>
 #include <allegro5\allegro_primitives.h>
 
-struct arrow_class
+struct spaceship_class
 {
 	int x,y;
 	int dir,speed;
 	int color;
 
-} arrow;
+} spaceship;
 
 #define MAXSPEED 3
 
-void create_arrow_bitmap(ALLEGRO_BITMAP * arrow_bmp[], ALLEGRO_DISPLAY *display);
-void erase_arrow();
-void move_arrow();
+void create_spaceship_bitmap(ALLEGRO_BITMAP * spaceship_bmp[], ALLEGRO_DISPLAY *display);
+void erase_spaceship();		
+void move_spaceship();
 void up();
 void down();
 void right();
@@ -22,7 +25,7 @@ void left();
 int main(void)
 {
 
-	ALLEGRO_BITMAP *arrow_bmp[4]={NULL,NULL,NULL,NULL};
+	ALLEGRO_BITMAP *spaceship_bmp[4]={NULL,NULL,NULL,NULL};
 	//variables
 	int width = 640;
 	int height = 480;
@@ -34,18 +37,18 @@ int main(void)
 
 
 	//program init
-	if(!al_init())										//initialize Allegro
+	if(!al_init())									
 		return -1;
 
-	display = al_create_display(width, height);			//create our display object
+	display = al_create_display(width, height);		
 
-	if(!display)										//test display object
+	if(!display)										
 		return -1;
 
 	//addon init
 	al_install_keyboard();
 	al_init_primitives_addon();
-	create_arrow_bitmap(arrow_bmp, display);
+	create_spaceship_bitmap(spaceship_bmp, display);
 
 
 	al_set_target_bitmap(al_get_backbuffer(display));
@@ -67,56 +70,83 @@ int main(void)
 				break;
 			}
 		}
-		al_draw_bitmap(arrow_bmp[0], width / 2, 100, 0);
-		al_draw_bitmap(arrow_bmp[1], width / 2, 200, 0);
-		al_draw_bitmap(arrow_bmp[2], width / 2, 300, 0);
-		al_draw_bitmap(arrow_bmp[3], width / 2, 400, 0);
+		al_draw_bitmap(spaceship_bmp[0], width / 2, 100, 0);
+		al_draw_bitmap(spaceship_bmp[1], width / 2, 200, 0);
+		al_draw_bitmap(spaceship_bmp[2], width / 2, 300, 0);
+		al_draw_bitmap(spaceship_bmp[3], width / 2, 400, 0);
 		al_flip_display();
 		al_clear_to_color(al_map_rgb(0,0,0));
 	}
 	for(int i=0; i<4; i++)
 	{
-		al_destroy_bitmap(arrow_bmp[i]);
+		al_destroy_bitmap(spaceship_bmp[i]);
 	}
 	al_destroy_event_queue(event_queue);
-	al_destroy_display(display);						//destroy our display object
+	al_destroy_display(display);					
 	system("pause");
 	return 0;
 }
 
-void create_arrow_bitmap(ALLEGRO_BITMAP * arrow_bmp[],ALLEGRO_DISPLAY *display)
+void create_spaceship_bitmap(ALLEGRO_BITMAP * spaceship_bmp[], ALLEGRO_DISPLAY *display)
 {
 	for(int i=0;i<4; i++)
 	{
-		arrow_bmp[i]=al_create_bitmap(32,32);   
-		if(!arrow_bmp[i]) {
+		spaceship_bmp[i]=al_create_bitmap(64,64);   
+		if(!spaceship_bmp[i]) {
 			exit(1);
 			al_destroy_display(display);
-
 		}
 
-		al_set_target_bitmap(arrow_bmp[i]);
-		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_set_target_bitmap(spaceship_bmp[i]);
+		al_clear_to_color(al_map_rgb(0, 0, 0)); 
 
-		int x = 15;
-		int y = 15;
-		al_draw_filled_rectangle(x-10, y-10, x+10, y+10, al_map_rgb(255, 255, 255));
+		int x = 32, y = 32;
+		int width = 12, height = 36; 
+		int wing_width = 10, wing_height = 20; 
+		int cockpit_width = 6, cockpit_height = 12; 
+		int antenna_length = 4; 
 
-		switch(i)
-		{
-		case 0: //Up
-			al_draw_filled_triangle(x-10,y-10,x+11,y-10,x,y-15,al_map_rgb(255, 0, 0));
-			break;
-		case 1://Right
-			al_draw_filled_triangle(x+11,y-11,x+11,y+11,x+15,y,al_map_rgb(255, 0, 0));
-			break;
-		case 2://Down
-			al_draw_filled_triangle(x-11,y+11,x+11,y+11,x,y+15,al_map_rgb(255, 0, 0));
-			break;
-		case 3: //Left
-			al_draw_filled_triangle(x-11,y-11,x-11,y+11,x-15,y,al_map_rgb(255, 0, 0));
-			break;
-		}
+		// body - white
+		al_draw_filled_rectangle(
+			x - width / 2, 
+			y - height / 2, 
+			x + width / 2, 
+			y + height / 2, 
+			al_map_rgb(255,255,255)
+		);
+
+		// nose - white 
+		al_draw_filled_triangle(
+			x - width / 2, y - height / 2, 
+			x + width / 2, y - height / 2, 
+			x, y - height / 2 - 10, 
+			al_map_rgb(255,255,255)
+		);
+
+		// left wing - red
+		al_draw_filled_triangle(
+			x - width / 2, y - height / 6, 
+			x - width / 2 - wing_width, y + wing_height / 2, 
+			x - width / 2, y + height / 2, 
+			al_map_rgb(255,0,0)
+		);
+
+		// right wing - red
+		al_draw_filled_triangle(
+			x + width / 2, y - height / 6, 
+			x + width / 2 + wing_width, y + wing_height / 2, 
+			x + width / 2, y + height / 2, 
+			al_map_rgb(255,0,0)
+		);
+
+		// blue cockpit 
+		al_draw_filled_rectangle(
+			x - cockpit_width / 2, 
+			y - height / 2 + 6, 
+			x + cockpit_width / 2, 
+			y - height / 2 + 6 + cockpit_height, 
+			al_map_rgb(0,120,255)
+		);
 	}
 }
 
