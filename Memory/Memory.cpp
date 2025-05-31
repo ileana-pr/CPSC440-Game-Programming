206cbf3b06bb53cc8a10e3898dc50d1e680919b1
cheddarqueso787 <cheddarqueso787@gmail.com>
2025-05-31 17:54:41 -0400

    add memory file strucure

---
+++ Memory/Memory.cpp
@@ -0,0 +1,10 @@
+//Name: Ileana Perez 
+//Programming assignment 2 
+// CPSC 440 
+
+#include <allegro5\allegro_ttf.h>
+#include <allegro5\allegro_primitives.h>	
+#include <allegro5\allegro_native_dialog.h> 
+#include "logic.h"
+
+#include " graphics.h"#include "logic.h"
