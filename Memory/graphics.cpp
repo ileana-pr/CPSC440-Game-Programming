bb61acda0c4eed1dc611d59c0960ebbe181ddb9a
cheddarqueso787 <cheddarqueso787@gmail.com>
2025-05-31 22:33:25 -0400

    saving updates to main

--- Memory/graphics.cpp
+++ Memory/graphics.cpp
@@ -3,22 +3,17 @@
 // CPSC 440 
 
 #include <allegro5\allegro.h>
+#include <cstdlib> 
+#include <iostream>
+#include <cstdio>
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_native_dialog.h> 
 #include "game.h"
 
+using namespace std;
 
-#define FPS 60
-
-//declare game finished 
-bool finished = false;
-//declare time out for the mismatch timer
-bool timeOut = false;
-int number_of_moves = 0;
-int pairs_found = 0;
-int pairs_remaining = 0;
 
 //get_mouse_input: use an if else statement to figure out wat box is being selected  
 void get_mouse_input(int &x, int &y);
@@ -28,8 +23,6 @@ void draw_objects(int x, int y);
 
 // get_shape: given an x and y location, it will return the shape that is supposed to be drawn. 
 int get_shape(int x, int y);
-
-
 
 
 // draw_octagon: draw the octagon shape
@@ -62,58 +55,99 @@ void draw_circle(int x, int y);
 // draw_square: draw the square shape
 void draw_square(int x, int y);
 
-// declare the 5 secondcountdown timer
-void* countdown_timer(ALLEGRO_THREAD* ptr, void* arg);
 
 int main(void)
 {
-    //init allegro
-	if(!al_init()) return -1;
-
     game memory_game;
+    bool done = false;
+    
+    ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_FONT *font = NULL;
 
     int width = 640;
 	int height = 520;
-	bool done = false;
 
-    //declare variables
+    //init allegro
+	if(!al_init()) 
+    {
+        printf("Error initializing allegro\n");
+        return -1;
+    }
 
-    ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	ALLEGRO_TIMER *timer = NULL;
-	ALLEGRO_FONT *font = NULL;
+    //create display
+	display = al_create_display(width, height);			//create our display object
+	if(!display)		
+        printf("Error creating display\n");								//test display object
+		return -1;
+
+    //init primitives
+	al_init_primitives_addon();
+
+    //init font
     al_init_font_addon();
     al_init_ttf_addon();
     font = al_load_font("GROBOLD.TTF", 16, 0);
     if (!font) return -1;
 
-    //program init
-	display = al_create_display(width, height);			//create our display object
-
-	if(!display)										//test display object
-		return -1;
-    //addon init
+    //init keyboard
 	al_install_keyboard();
-	al_init_primitives_addon();
 
-    //create game objects 
+    //init mouse
+    al_install_mouse();
 
-
-    // create timer
-	timer = al_create_timer(1.0 / FPS);
-
+    //create event queue
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	event_queue = al_create_event_queue();
-	al_register_event_source(event_queue, al_get_keyboard_event_source());
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+     //clear to color
+    al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_clear_to_color(al_map_rgb(0,0,0));
 
-    // thread to run the countdown timer
-	ALLEGRO_THREAD* timer_thread = al_create_thread(countdown_timer, NULL);
+    //register event source
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+
+
+    //create the game board
+
+    // fill the game board with 25 cards 
+
 
     while (!done) 
     {
+        // During the game play you must keep track of two click patterns. First click must select an object from the selection bar; the second must be on the draw grid.
+
+        //On the first mouse click you must set a global variable indicating the shape (enum type) that was selected
+
+        //On the second mouse click if it is a valid location you will set_shape and the draw_objects
+
+
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
+
+        if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+        {
+            get_mouse_input(ev.mouse.x, ev.mouse.y);
+        }
+
+
+
+
+
+
+
+
+
+        al_flip_display();
+        al_rest(5.0); 
+        al_destroy_display(display);
+        al_destroy_event_queue(event_queue);
+        al_destroy_timer(timer);
+        al_destroy_thread(timer_thread);
+        al_destroy_font(font);
+        
+        
         
         
     }
