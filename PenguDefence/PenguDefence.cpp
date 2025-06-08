d6b896a0eae4515b6f6805d1cf907f3bba23d5e3
cheddarqueso787 <cheddarqueso787@gmail.com>
2025-06-08 00:38:41 -0400

    add pengu defence files

---
+++ PenguDefence/PenguDefence.cpp
@@ -0,0 +1,17 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+
+// The object of the game is to defend your iceberg from dropping penguins by firing snowballs at them. You will use the left and right keys to aim, and the spacebar to fire. If enough penguins land on the iceberg, it will sink. The penguins will drop from the top of the screen, and you will need to fire at them before they reach the iceberg. 
+
+
+
+
+
+
+
+
+
---
+++ PenguDefence/penguin.h
@@ -0,0 +1,32 @@
+#ifndef PENGUINH
+#define PENGUINH
+#include iceberg.h
+
+class penguin 
+{
+    public:
+    penguin();
+    ~penguin();
+    void draw_penguin();
+    penguin start_penguin(int width, int height);
+    void update_penguin();
+    void collide_penguin(iceberg &iceberg);
+    int get_x();
+    int get_y();
+    int get_bound_x();
+    int get_bound_y();
+    bool get_live();
+    void set_live(bool live);
+
+    private:
+    int x;
+    int y;
+    int bound_x;
+    int bound_y;
+    bool live;
+    int speed;
+    ALLEGRO_BITMAP *image;
+};
+
+
+#endif
